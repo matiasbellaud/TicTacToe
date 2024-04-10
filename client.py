ae39300522f4e24b8422593b2862fa4b75ca474b
fevan <evan.ferron53@gmail.com>
2024-04-10 09:05:02 +0200

    :bug: | resolve bug for tcp

--- client.py
+++ client.py
@@ -1,12 +1,12 @@
 import socket
 
 host='192.168.229.110' #client ip
-port = 4005
+port = 400
 
 server = ('192.168.229.30', 4000)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((host, port))
+client.connect(server)
 print ('Connexion vers ' + host + ':' + str(port) + ' reussie.')
 
 message = input("-> ")
