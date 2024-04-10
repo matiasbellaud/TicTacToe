54266170ba03e5c2f83f533a9ece04d965ebb6a0
fevan <evan.ferron53@gmail.com>
2024-04-10 10:44:35 +0200

    :wrench: | edit the way to import

--- client.py
+++ client.py
@@ -1,10 +1,10 @@
 import socket
-import Display
-import inputList
-import IsGameEnd
+from Display import Display
+from inputList import inputList
+from IsGameEnd import IsGameEnd
 import os
-import listToString
-import StringToListe
+from listToString import listToString
+from StringToListe import StringToListe
 
 host='192.168.229.110' #client ip
 port = 4005
@@ -20,8 +20,8 @@ game = [[0,0,0],[0,0,0],[0,0,0]]
 while True:
     Display.Display(game)
 
-    game = inputList.inputList(game,1)
-    gameStatus = IsGameEnd.IsGameEnd(game)
+    game = inputList(game,1)
+    gameStatus = IsGameEnd(game)
     if gameStatus != 0:
         match gameStatus:
             case 1:
@@ -33,14 +33,14 @@ while True:
             case 3:
                 print("no cells left")
                 break
-    print(listToString.listToString(game))
+    print(listToString(game))
     Display.Display(game)
-    client.sendto(listToString.listToString(game).encode('utf-8'), server)
+    client.sendto(listToString(game).encode('utf-8'), server)
     data, addr = client.recvfrom(1024)
     os.system("clear")
     data = data.decode('utf-8')
-    game = StringToListe.StringToListe(data)
-    gameStatus = IsGameEnd.IsGameEnd(game)
+    game = StringToListe(data)
+    gameStatus = IsGameEnd(game)
     if gameStatus != 0:
         match gameStatus:
             case 1:
