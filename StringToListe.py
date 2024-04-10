f8608ed6b8584f96bd0be43518aaeb7d6cc3e61f
matiasbellaud <bellaud.matias@gmail.com>
2024-04-10 10:30:23 +0200

    string to list

---
+++ StringToListe.py
@@ -0,0 +1,9 @@
+def inputList(liste):
+    liste2 = [[0,0,0],[0,0,0],[0,0,0]]
+    index=0
+    for i in range(3) :
+        for j in range(3) :
+            liste2[i][j] = liste[index]
+            index = index+1
+
+    return (liste2)
