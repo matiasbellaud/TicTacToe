1f443244093ae111182886224d4ab6aabf84540e
matiasbellaud <bellaud.matias@gmail.com>
2024-04-10 09:40:25 +0200

    input fucntion

---
+++ inputList.py
@@ -0,0 +1,18 @@
+def inputList(liste: list, player) -> list:
+    message = input("nombre entre 1 et 9 -> ")
+    if message.isdigit() == True :
+        if int(message) <= 9 and int(message) >= 1 :
+            tic = 0
+            for i in range(3):
+               for j in range(3): 
+                   tic = tic + 1
+                   if tic == int(message):
+                       liste[i][j] = player              
+        else :
+            print("entré non valide")
+            return inputList(liste,player)
+    else :
+        print("entré non valide")
+        return inputList(liste,player)
+
+    return (liste)
--- server.py
+++ server.py
@@ -1,40 +1,24 @@
 import socket
 
+from inputList import inputList
+
 def Main():
 
-    s = socket.socket( socket.SOCK_DGRAM)
-    host = '192.168.229.30' #Server ip
-    port = 4000
-    s.bind(('192.168.229.30', 4000))
-    print("test1")
-    s.listen(5)
-    print("test2")
-    client_socket, client_address = s.accept()
-    print("test3")
+    client = ('192.168.229.110', 4005)
+
+    server = socket.socket( socket.SOCK_DGRAM)
+    server.bind(('192.168.229.30', 4000))
+    server.listen(200)
+
+    client_socket, client_address = server.accept()
     donnees = client_socket.recv(1024).decode('utf-8')
-    print("test4")
     print("Données reçues du client :", donnees)
+    message = input("-> ")
+    server.sendto(message.encode('utf-8'), client)
 
-    # Fermer la connexion avec le client
     client_socket.close()
+    server.close()
 
-    # Fermer le socket serveur
-    s.close()
-    # s.bind((host, port))
-
-
-    # print("Server Started")
-    # while True:
-        
-    #     data, addr = s.recvfrom(1024)
-    #     print(addr)
-    #     data = data.decode('utf-8')
-    #     print("Message from: " + str(addr))
-    #     print("From connected user: " + data)
-    #     data = data.upper()
-    #     print("Sending: " + data)
-    #     s.sendto(data.encode('utf-8'), addr)
-    # s.close()
    
 if __name__=='__main__':
     Main()
