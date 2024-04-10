f9b04adf8ac91cb68d7a7f34f7b1bff3898d4297
fevan <evan.ferron53@gmail.com>
2024-04-10 10:38:51 +0200

    :bug: | fix encode list bug

--- client.py
+++ client.py
@@ -3,7 +3,8 @@ import Display
 import inputList
 import IsGameEnd
 import os
-import TicTacToe.listToString as listToString
+import listToString
+import StringToListe
 
 host='192.168.229.110' #client ip
 port = 4005
@@ -32,12 +33,23 @@ while True:
             case 3:
                 print("no cells left")
                 break
-    
+    print(listToString.listToString(game))
+    Display.Display(game)
     client.sendto(listToString.listToString(game).encode('utf-8'), server)
-
-    os.system("clear")
     data, addr = client.recvfrom(1024)
+    os.system("clear")
     data = data.decode('utf-8')
-    print(data)
-    message = input("-> ")
+    game = StringToListe.StringToListe(data)
+    gameStatus = IsGameEnd.IsGameEnd(game)
+    if gameStatus != 0:
+        match gameStatus:
+            case 1:
+                print("you have win")
+                break
+            case 2:
+                print("the other have win")
+                break
+            case 3:
+                print("no cells left")
+                break
 
